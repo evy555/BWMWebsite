54cedae56dca54ab80269db07e23aa8586d9062f
evy555 <evansj556@yahoo.com>
2017-06-20 13:50:58 -0700

    Tabs and Scrolling
    Added Tabs to About Us and Investment Strategies. Will be used to
    display information from inserted HTML pages. Also fixed scrolling to
    fit at the top of each section.

--- static/js/master.js
+++ static/js/master.js
@@ -1,14 +1,32 @@
 // Tab Functionality
-jQuery(document).ready(function() {
-    jQuery('.tabs .tab-links a').on('click', function(e)  {
-        var currentAttrValue = jQuery(this).attr('href');
+// jQuery(document).ready(function() {
+//     jQuery('.tabs .tab-links a').on('click', function(e)  {
+//         var currentAttrValue = jQuery(this).attr('href');
+//
+//         // Show/Hide Tabs
+//         jQuery('.tabs ' + currentAttrValue).fadeIn(400).siblings().hide();
+//
+//         // Change/remove current tab to active
+//         jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+//
+//         e.preventDefault();
+//     });
+// });
 
-        // Show/Hide Tabs
-        jQuery('.tabs ' + currentAttrValue).fadeIn(400).siblings().hide();
+function openTab(evt, tabName, section) {
+  var i, tabcontent, tablinks;
 
-        // Change/remove current tab to active
-        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+  tabcontent = document.getElementsByClassName('tabcontent' + section);
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
 
-        e.preventDefault();
-    });
-});
+  tablinks = document.getElementsByClassName("tablinks" + section);
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+
+}
